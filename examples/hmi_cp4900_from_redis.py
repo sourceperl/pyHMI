23b9aa4abfa30aba0f90d8bcd1ff3fc7476410b1
sourceperl <loic.celine@free.fr>
2016-11-10 14:12:19 +0100

    fix duplicate CP_STATE

--- examples/hmi_cp4900_from_redis.py
+++ examples/hmi_cp4900_from_redis.py
@@ -29,7 +29,6 @@ class Tags(object):
     CP_FLOW_GAS = Tag(0, src=Devices.redis, ref={'type': 'float', 'key': 'cp4900:flow_gas'})
     CP_ANALYSIS_FAULT = Tag(0, src=Devices.redis, ref={'type': 'int', 'key': 'cp4900:analysis_fault'})
     CP_SENSOR_FAULT = Tag(0, src=Devices.redis, ref={'type': 'int', 'key': 'cp4900:sensor_fault'})
-    CP_STATE = Tag(0, src=Devices.redis, ref={'type': 'str', 'key': 'cp4900:state'})
     RPI_SIGFOX_SINCE_TX = Tag(0, src=Devices.redis, ref={'type': 'int', 'key': 'rpi_sigfox:since_tx'})
 
     @classmethod
