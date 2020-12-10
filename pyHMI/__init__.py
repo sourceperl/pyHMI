da94537ce91f58c78fe2d8a2fe66e1293f0cbf75
sourceperl <loic.celine@free.fr>
2020-12-10 09:03:32 +0100

    set version to 0.0.6

--- pyHMI/__init__.py
+++ pyHMI/__init__.py
@@ -3,10 +3,10 @@
 # Python package: pyHMI
 #        Version: _dev_
 #        Website: https://github.com/sourceperl/pyHMI
-#           Date: 2016-05-14
+#           Date: 2020-12-10
 #        License: MIT (http://http://opensource.org/licenses/mit-license.php)
 #    Description: A set of class for easy build tkinter HMI with Python
 #        Charset: utf-8
 
 __all__ = ()
-__version__ = '0.0.5'
+__version__ = '0.0.6'
